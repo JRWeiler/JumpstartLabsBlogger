e69511b6b7470fc15ea32eaed5cd14d7e6ddc634
Josh <jweiler@cn.edu>
2015-06-09 14:42:11 +0000

    Added gems and image uploading

--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608192742) do
+ActiveRecord::Schema.define(version: 20150609141828) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_content_name"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: true do |t|
